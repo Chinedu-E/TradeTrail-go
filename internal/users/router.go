d79d9870e2707a994a9c25e3ec13465405945bd0
chinedu <crankyekeruche@gmail.com>
2023-03-07 17:26:52 -0500

    added query param for getuser endpoint

--- internal/users/router.go
+++ internal/users/router.go
@@ -6,8 +6,8 @@ import (
 
 func AddUserRoutes(app *gin.Engine, controller *UserController) {
 	users := app.Group("/users")
-	users.GET("/all", controller.getAllUsers)
-	users.GET("/user", controller.getUser)
+	users.GET("/", controller.getAllUsers)
+	users.GET("/:id", controller.getUser)
 	users.POST("/login", controller.login)
 	users.POST("/register", controller.register)
 }
