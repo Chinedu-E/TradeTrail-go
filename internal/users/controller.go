47d29a9e79f0afdfa901aa2c2614662a950b47e9
chinedu <crankyekeruche@gmail.com>
2023-03-07 17:15:59 -0500

    added generateJWT functions

--- internal/users/controller.go
+++ internal/users/controller.go
@@ -1,7 +1,10 @@
 package users
 
 import (
+	"time"
+
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
 )
 
 type UserController struct {
@@ -23,3 +26,16 @@ func (t *UserController) login(c *gin.Context) {
 func (t *UserController) getUser(c *gin.Context) {}
 
 func (t *UserController) getAllUsers(c *gin.Context) {}
+
+func generateJWT(username string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"username":   username,
+		"exp":        time.Now().Add(time.Hour * 24).Unix(),
+		"authorized": true,
+	})
+	tokenString, err := token.SignedString([]byte("secret"))
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
