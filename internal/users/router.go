ad78561f77c3dd0a02eb06421ee1e6758d164812
chinedu <crankyekeruche@gmail.com>
2023-03-07 06:20:09 -0500

    function rename; replaced todo -> user

--- internal/users/router.go
+++ internal/users/router.go
@@ -4,7 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func AddTodoRoutes(app *gin.Engine, controller *UserController) {
+func AddUserRoutes(app *gin.Engine, controller *UserController) {
 	users := app.Group("/users")
 	users.GET("/all", controller.getAllUsers)
 	users.GET("/user", controller.getUser)
