ec2578ebe376dfc077071c2412f054241020735a
Nedu Ekeruche <crankyekeruche@gmail.com>
2023-03-09 04:20:31 -0500

    added router function

--- internal/sessions/router.go
+++ internal/sessions/router.go
@@ -1,1 +1,10 @@
 package sessions
+
+import "github.com/gin-gonic/gin"
+
+func AddSessionRoutes(app *gin.Engine, controller *SessionController) {
+	session := app.Group("/session")
+	session.GET("/", controller.getSession)
+	session.POST("/", controller.createSession)
+
+}
