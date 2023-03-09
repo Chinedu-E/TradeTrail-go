d0ca01ec872e5a890f707d86e617c85227cb98c3
Nedu Ekeruche <crankyekeruche@gmail.com>
2023-03-09 03:41:55 -0500

    two session tables structs created

--- internal/sessions/storage.go
+++ internal/sessions/storage.go
@@ -1,1 +1,31 @@
 package sessions
+
+import (
+	"gorm.io/gorm"
+)
+
+type Session struct {
+	gorm.Model
+	HostId          int
+	Duration        int
+	Symbol          string
+	StartingBalance float64
+	NumParticipants int
+	AgainstAgent    bool
+}
+
+type SessionParticipants struct {
+	gorm.Model
+	SessionId    int
+	UserId       int
+	StartBalance float64
+	NumTrades    int
+	EndBalance   float64
+	NetShares    float64
+	Profit       float64
+	Agent        bool
+}
+
+type SessionStorage struct {
+	db *gorm.DB
+}
