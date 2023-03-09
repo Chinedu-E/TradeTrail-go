e6b6c3eb499951fda523539ac57dc5d3718f72f3
Nedu Ekeruche <crankyekeruche@gmail.com>
2023-03-09 04:21:58 -0500

    added db handler functions

--- internal/portfolios/storage.go
+++ internal/portfolios/storage.go
@@ -1,1 +1,76 @@
 package portfolios
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Portfolio struct {
+	gorm.Model
+	UserId          int
+	Type            string
+	SecuritiesType  string
+	StartingBalance float64
+	Balance         float64
+	Value           float64
+	AutoManaged     bool
+}
+
+type PortfolioValues struct {
+	gorm.Model
+	PortfolioId int
+	Date        time.Time
+	Value       float64
+}
+
+type UserPortfolio struct {
+	gorm.Model
+	PortfolioId int
+	Symbol      string `gorm:"not null"`
+	Shares      float64
+}
+
+type PortfolioStorage struct {
+	db *gorm.DB
+}
+
+func NewPortfolioStorage(db *gorm.DB) *PortfolioStorage {
+	return &PortfolioStorage{
+		db: db,
+	}
+}
+
+func (s *PortfolioStorage) GetPortfolio(id int) (*Portfolio, error) {
+	// Get the portfolio from the "Portfolios" table
+	var portfolio *Portfolio
+	if err := s.db.Where("id = ?", id).First(&portfolio).Error; err != nil {
+		return nil, err
+	}
+
+	return portfolio, nil
+}
+
+func (s *PortfolioStorage) GetAllPortfolios() ([]*Portfolio, error) {
+	// Get the portfolio from the "Portfolios" table
+	var portfolios []*Portfolio
+	if err := s.db.Find(&portfolios).Error; err != nil {
+		return nil, err
+	}
+
+	return portfolios, nil
+}
+
+func (s *PortfolioStorage) GetAllUserPortfolios(id int) ([]*Portfolio, error) {
+	// Get the portfolio from the "Portfolios" table
+	var portfolios []*Portfolio
+
+	// Get balance from the "Portfolios" table
+	err := s.db.Table("portfolios").Where("user_id = ?", id).Scan(&portfolios).Error
+	if err != nil {
+
+		return nil, err
+	}
+
+	return portfolios, nil
+}
