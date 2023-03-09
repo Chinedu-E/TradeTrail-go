f8b94edf6739af39fb77f6c9780b215a2727872c
Nedu Ekeruche <crankyekeruche@gmail.com>
2023-03-09 04:20:13 -0500

    added endpoint handlers

--- internal/sessions/controller.go
+++ internal/sessions/controller.go
@@ -1,1 +1,47 @@
 package sessions
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type SessionController struct {
+	storage *SessionStorage
+}
+
+func NewSessionController(storage *SessionStorage) *SessionController {
+	return &SessionController{
+		storage: storage,
+	}
+}
+
+func (t *SessionController) getSession(c *gin.Context) {
+	id := c.Query("id")
+	sessionId, err := strconv.Atoi(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+	}
+
+	session, err := t.storage.GetSession(sessionId)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+	c.JSON(http.StatusOK, session)
+}
+
+func (t *SessionController) createSession(c *gin.Context) {
+	var session *Session
+	if err := c.ShouldBindJSON(&session); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+	}
+	if _, err := t.storage.Create(session); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+	c.JSON(http.StatusOK, session)
+}
