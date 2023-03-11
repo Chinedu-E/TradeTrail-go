abd68593c12bed03820f377160f173ddb177f4a5
chinedu <crankyekeruche@gmail.com>
2023-03-11 13:23:26 -0500

    group name typo

--- internal/leaderboards/router.go
+++ internal/leaderboards/router.go
@@ -3,7 +3,7 @@ package leaderboards
 import "github.com/gin-gonic/gin"
 
 func AddLeaderboardRoutes(app *gin.Engine, controller *LeaderBoardController) {
-	leaderboard := app.Group("/users")
+	leaderboard := app.Group("/leaderboard")
 	leaderboard.GET("/", controller.getLeaderBoard)
 	leaderboard.POST("/", controller.addToLeaderBoard)
 }
