9fc0f42ae8fb9d0e2fb0a42bac5a62cc013bf116
chinedu <crankyekeruche@gmail.com>
2023-03-11 13:08:56 -0500

    created leaderboard routing

--- internal/leaderboards/router.go
+++ internal/leaderboards/router.go
@@ -1,1 +1,9 @@
 package leaderboards
+
+import "github.com/gin-gonic/gin"
+
+func AddLeaderboardRoutes(app *gin.Engine, controller *LeaderBoardController) {
+	leaderboard := app.Group("/users")
+	leaderboard.GET("/", controller.getLeaderBoard)
+	leaderboard.POST("/", controller.addToLeaderBoard)
+}
