d2789c2982b997a1504d95f508ab824cb88fe3e3
chinedu <crankyekeruche@gmail.com>
2023-03-11 13:12:07 -0500

    added potfolio routing function

--- internal/portfolios/router.go
+++ internal/portfolios/router.go
@@ -1,1 +1,11 @@
 package portfolios
+
+import "github.com/gin-gonic/gin"
+
+func AddPortfolioRoutes(app *gin.Engine, controller *PortfolioController) {
+	portfolio := app.Group("/portfolio")
+	portfolio.GET("/:id", controller.getPortfolio)
+	portfolio.GET("/user/:id", controller.getAllUserPortfolios)
+
+	portfolio.POST("/", controller.createPortfolio)
+}
