9e29dd916e6fc9c22e768ec0d24af762980b5405
chinedu <crankyekeruche@gmail.com>
2023-03-11 13:11:03 -0500

    created transaction storage model

--- internal/transactions/storage.go
+++ internal/transactions/storage.go
@@ -1,1 +1,25 @@
 package transactions
+
+import "gorm.io/gorm"
+
+type Transaction struct {
+	gorm.Model
+	SourceId int
+	Symbol   string
+	Shares   float64
+	BoughtAt float64
+	Type     string
+	Source   string
+}
+
+type TransactionStorage struct {
+	db *gorm.DB
+}
+
+func NewTransactionStorage(db *gorm.DB) *TransactionStorage {
+	return &TransactionStorage{
+		db: db,
+	}
+}
+
+func CreateTransaction() {}
