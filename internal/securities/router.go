7c846ce64efad337084c12b792cee483ccf1a143
chinedu <crankyekeruche@gmail.com>
2023-03-12 06:13:49 -0400

    added endpoint routes

--- internal/securities/router.go
+++ internal/securities/router.go
@@ -1,1 +1,11 @@
 package securities
+
+import "github.com/gin-gonic/gin"
+
+func AddSecuritiesRoutes(app *gin.Engine, controller *SecurityController) {
+	prices := app.Group("/prices")
+	prices.GET("/info", controller.getSecurityInfo)
+	prices.GET("/latest", controller.getLatestPrice)
+	prices.GET("/latest/:limit", controller.getLatestPrices)
+	prices.GET("/history", controller.getSymbolHistory)
+}
