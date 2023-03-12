ab70e0a1d9d128128584e97671f273f52933e48e
chinedu <crankyekeruche@gmail.com>
2023-03-12 05:12:12 -0400

    added endpoint routing

--- internal/transactions/router.go
+++ internal/transactions/router.go
@@ -1,1 +1,12 @@
 package transactions
+
+import "github.com/gin-gonic/gin"
+
+func AddTransactionRoutes(app *gin.Engine, controller *TransactionController) {
+	transactions := app.Group("/transactions")
+	transactions.GET("/portfolio/:id", controller.getPortfolioTransactions)
+	transactions.GET("/user/:id", controller.getUserTransactions)
+
+	transactions.POST("/buy", controller.buy)
+	transactions.POST("/buy", controller.sell)
+}
